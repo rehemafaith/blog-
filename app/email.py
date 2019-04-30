27a2ad665594455dc4ee19b1c8a424abf874fe9f
rehemafaith <rehemafaith01@gmail.com>
2019-04-30 18:44:20 +0300

    fixed indentation

--- app/email.py
+++ app/email.py
@@ -8,4 +8,4 @@ def mail_message(subject,template,to,**kwargs):
   email = Message(subject, sender = "rehemafaith01@gmail.com",recipients=[to])
   email.body = render_template(template + ".txt",**kwargs)
   
-  mail.send(email)
+  mail.send(email)  
